a697c796d93d89fd10f02a91300c56740fd865e2
abdulhadi240 <ah912425@gmail.com>
2023-11-13 14:37:25 +0500

    api routes

--- pages/api/All.ts
+++ pages/api/All.ts
@@ -1,14 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import {client} from '../../components/Datafetching'; // Import the Sanity client
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req, res) => {
-   try {
-     // Fetch all blogs from Sanity
-     const blogs = await client.fetch(`*[_type == "post"]`);
-     res.status(200).json(blogs);
-   } catch (error) {
-     console.error(error);
-     res.status(500).json({ error: 'An error occurred while'})
-   }
 
+type ResponseData = {
+  message: string;
+};
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  const authHeader = req.headers.authorization;
+  const name = req.query.title as string | undefined;
+  const body = req.body;
+  console.log(authHeader);
+  
+  if (authHeader == 'Bearer 123456789') {
+    try {
+      let data: any;
+      if (!name) {
+        data = await client.fetch(`*[_type == 'post']`,{
+          next:{
+            revalidate:10
+          }
+        });
+      } else {
+        data = await client.fetch(`*[_type == 'post' && Title == '${name}']`),{
+          next:{
+            revalidate:10
+          }
+        };
+      }
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching data' });
+    }
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
 }
